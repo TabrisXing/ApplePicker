6d4416029cb6057c4751e706939ec0a323de8fdf
TabrisXing <xingtianx@gmail.com>
2020-02-05 22:42:50 -0500

    drop apple normally

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -20,17 +20,30 @@ public class AppleTree : MonoBehaviour
     // Rate at which Apples will be instantiated
 
     public float secondsBetweenAppleDrops = 1f;
+
     void Start()
     {
         // Dropping apples every second
+        Invoke("DropApple", 2f);
     }
+
+    void DropApple()
+    { 
+        GameObject apple = Instantiate<GameObject>(applePrefab
+        ); 
+        apple.transform.position =
+        transform.position; 
+        Invoke("DropApple", secondsBetweenAppleDrops
+        );
+    }
+
 
     void Update()
     {
         // Basic Movement
-        Vector3 pos = transform.position; // b
-        pos.x += speed * Time.deltaTime; // c
-        transform.position = pos; // d
+        Vector3 pos = transform.position; 
+        pos.x += speed * Time.deltaTime; 
+        transform.position = pos; 
 
         // Changing Direction
         if (pos.x < -leftAndRightEdge)

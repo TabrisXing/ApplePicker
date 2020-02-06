477354800506816d5054e41295f015c9b027ff87
TabrisXing <xingtianx@gmail.com>
2020-02-05 23:01:34 -0500

    move basket with mouse

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -8,17 +8,11 @@ public class Basket : MonoBehaviour
     void Update()
     {
         // Get the current screen position of the mouse from Input
-        Vector3 mousePos2D =
-        Input.mousePosition; // a
-                             // The Camera's z position sets how far to push the mouse into 3D
-        mousePos2D.z = -
-        Camera.main.transform.position.z; // b
-                                          // Convert the point from 2D screen space into 3D game world spac
-        e
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D)
-        ; // c
-          // Move the x position of this Basket to the x position of the Mo
-        use
+        Vector3 mousePos2D =Input.mousePosition; 
+
+        mousePos2D.z = -Camera.main.transform.position.z;
+                                         
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D); 
         Vector3 pos = this.transform.position;
         pos.x = mousePos3D.x;
         this.transform.position = pos;
